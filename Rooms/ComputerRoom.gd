31c3ce41909fad8cfea0f433d1a1df525dde533a
Minivera <guillaume@manifold.co>
2020-07-12 09:59:47 -0400

    Small room fixes

--- Rooms/ComputerRoom.gd
+++ Rooms/ComputerRoom.gd
@@ -4,4 +4,5 @@ extends Control
 export (int) var infection_level = false setget _set_infection_level
 
 func _set_infection_level(new_infection_level):
+	infection_level = new_infection_level
 	$Background/BasicRoom.infection_level = infection_level
