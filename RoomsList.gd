23b24adb9100a263ac744adac6685b035f2da1ac
Minivera <guillaume@manifold.co>
2020-07-12 12:22:34 -0400

    Added all the rooms

--- RoomsList.gd
+++ RoomsList.gd
@@ -10,17 +10,42 @@ func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-func _on_Computer_button_down():
-	_handle_click("headquarters")
-
-func _on_Pod_button_down():
-	_handle_click("living")
-
 # Play sound effect
 func _handle_click(type):
 	$ClickSfx.play()
 	emit_signal("room_pressed", type)
+
+func _on_Computer_button_down():
+	_handle_click("headquarters")
+
+
+func _on_Living_button_down():
+	_handle_click("living")
+
+
+func _on_Farm_button_down():
+	_handle_click("farm")
+
+
+func _on_Reactor_button_down():
+	_handle_click("reactor")
+
+
+func _on_Energy_button_down():
+	_handle_click("energy")
+
+
+func _on_Fabricator_button_down():
+	_handle_click("fabricator")
+
+
+func _on_Laboratory_button_down():
+	_handle_click("lab")
+
+
+func _on_Pump_button_down():
+	_handle_click("pump")
+
+
+func _on_Cryo_button_down():
+	_handle_click("cryo")
